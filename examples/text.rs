a5afb4daa31e4179aa0d5041a3002bb6d543e64d
Blair Noctis <n@sail.ng>
2024-03-15 04:12:16 +0800

    impl: improve text example a little

--- examples/text.rs
+++ examples/text.rs
@@ -1,12 +1,24 @@
+use std::time::Instant;
+
 const SECRET: &str = "Test Secret Please Ignore";
 
 fn main() {
+	let start = Instant::now();
+	let mut rand = 0;
+	for _ in 0..1024 {
+		rand += start.elapsed().as_micros() % 1024;
+	}
+
 	let mut buffer = String::new();
-	blindcopy::text(SECRET).unwrap();
-	print!("Paste back here: ");
+
+	let secret = format!("{SECRET}: {}", start.elapsed().as_micros() + rand);
+
+	blindcopy::text(&secret).unwrap();
+	println!("Your super secret string is: {secret:?}");
+	println!("It should have been copied. Paste back below to verify: ");
 	std::io::stdin().read_line(&mut buffer).unwrap();
-	println!();
-	if buffer.trim() == SECRET {
+	buffer = buffer.trim().to_string();
+	if buffer == secret {
 		println!("It's the same! Now check your clipboard manager.");
 	} else {
 		println!("It's not the same. You pasted: {buffer:?}");
